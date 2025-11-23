3d7cca4490b4838f3c1d13076b39c50401217386
verdsouza <officialcapitalroot@gmail.com>
2025-11-23 13:02:02 +0530

    ok kil kil kill

--- next.config.js
+++ next.config.js
@@ -1,61 +1,13 @@
-//   // next.config.js
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-  
-//   images: {
-//     domains: ['movieondemand.vercel.app', 'short.icu'],
-//   },
-// }
-
-// module.exports = nextConfig
-
-
-
-
-
-// next.config.js
+  // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['i.ytimg.com', 'img.youtube.com', 'www.dailymotion.com', 'dmcdn.net', 'abyss.to', 'movieondemand.vercel.app', 'short.icu'],
-    formats: ['image/webp', 'image/avif'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  // experimental: {
+  //   appDir: true,
+  // },
+    images: {
+    domains: ['i.ibb.co', 'imgbb.com', 'short.icu'],
+    unoptimized: true
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY'
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block'
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin'
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains'
-          }
-        ],
-      },
-    ]
-  },
-  compress: true,
-  poweredByHeader: false,
-  generateEtags: false,
   trailingSlash: true,
 }
 
