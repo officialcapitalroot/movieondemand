b74811ad4d8b93ab4d2e867ef77b57b8bb680001
verdsouza <officialcapitalroot@gmail.com>
2025-11-23 11:57:14 +0530

    NEXT JS  IMAGES

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -22,8 +22,7 @@ export default function MovieCard({ movie }) {
             width={300}
             height={450}
             quality={100}
-            fetchpriority="high"
-            loading="eager"
+            priority
             className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
           />
           
